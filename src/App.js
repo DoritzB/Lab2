20d82a31ae9275be8e3a6263c7a420834dd23e1f
Dorian <doritzb@gmail.com>
2023-05-02 14:16:57 +0300

    creat route finisat lab7

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 import BasicButtons from "./mybutton";
 import MediaCard from "./card";
 import BasicTextFields from "./Text";
 import BasicSelect from "./Select";
 import RadioButtonsGroup from "./Radio";
+import ToDoList from "./toDoList";
+import LoginButton from "./login";
+import MenuApi from "./ApiMenu";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h>Doritz Botosanu CR-201</h>
-      <BasicButtons/>
-        <div className="Dreapta">
-          <MediaCard/>
-        <MediaCard/>
-        <MediaCard/>
-        <MediaCard/>
-        <MediaCard/>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+              <li>
+                <Link to="/books">Books</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route
+                path="/home"
+                element={
+                  <div className="App">
+                    <h2>Doritz Botosanu CR-201</h2>
+                    <BasicButtons/>
+                    <LoginButton/>
+                    <div className="Dreapta">
+                      <MediaCard/>
+                      <MediaCard/>
+                      <MediaCard/>
+                      <MediaCard/>
+                      <MediaCard/>
+                    </div>
+                    <BasicTextFields/>
+                    <BasicSelect/>
+                    <RadioButtonsGroup/>
+                    <ToDoList/>
+                  </div>
+                }
+            />
+            <Route path="/books" element={<MenuApi />} />
+          </Routes>
         </div>
-      <BasicTextFields/>
-        <BasicSelect/>
-        <RadioButtonsGroup/>
-    </div>
+      </Router>
   );
 }
 
